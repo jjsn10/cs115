d247888d4835a1e48aa5213a3faca8c3da81d511
jjsn10 <sanche37@ensign.edu>
2023-04-19 14:36:21 -0600

    Remove some type of files from gitignore

--- Labs/src/Lab1i.java
+++ Labs/src/Lab1i.java
@@ -1,17 +1,30 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Collections;
+
+/**
+* File: Lab1i.java
+* Description: Java Basics Review
+* Instructor's Name: Jeff Light
+*
+* @author Jared Sanchez
+* @since 04/19/2023
+*/
 
 public class Lab1i {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//ArrayList numbers to store numbers provide by the user
 		ArrayList <Integer> numbers = new ArrayList<Integer>();
+		//boolean status: variable to handle if we are in or out of the do while
 		boolean status = false;
+		//Scanner input: variable to get the input from the console.
 		Scanner input = new Scanner(System.in);
 		
 		
 		System.out.println("In this program, you will enter a list of numbers.");
+		
 		do {
 			System.out.println("Please enter a number (or hit the <Enter> key to stop):");
 			try {
@@ -31,6 +44,13 @@ public class Lab1i {
 		System.out.println("The biggest number in the list is:"+largestNumber(numbers));
 		
 	}
+	/**
+	* Method Name: addNumbers(ArrayList<Integer> numbers)
+	* Description: This function sum all numbers added to the ArrayList numbers
+	* Returns: Returns the sum of the integer numbers added to the ArrayList numbers
+	*
+	* @param ArrayList<Integer> numbers ArrayList of Integer numbers filled at the main method
+	*/
 	public static int addNumbers(ArrayList<Integer> numbers) {
 		int sum = 0;
 		for(int i=0; i<numbers.size(); i++) {
