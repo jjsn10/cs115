9de7382e40e64440d8b75c0fd9f531e4e2af67ab
jjsn10 <san14038@byui.edu>
2023-05-13 20:45:52 -0600

    update DessertShop

--- Labs/src/DessertShop/DessertShop.java
+++ Labs/src/DessertShop/DessertShop.java
@@ -1,27 +1,82 @@
 package DessertShop;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DessertShop {
 	
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		Order orders = new Order();
 		
 		//Creating Instances of Candy, Cookie, IceCream, and Sunday
-		Candy c1 = new Candy("Candy Corn",1.5,0.25);
-		Candy c2 = new Candy("Gummy Bears",0.25,0.35);
-		Cookie ck1 = new Cookie("Chocolate Chip",6,3.99);
-		IceCream i = new IceCream("Pistachio",2,0.79);
-		Sundae s = new Sundae("Vanilla",3,0.69,"Hot Fudge",1.29);
-		Cookie ck2 = new Cookie("Oatmeal Raisin",2,3.45);
+				Candy c1 = new Candy("Candy Corn",1.5,0.25);
+				Candy c2 = new Candy("Gummy Bears",0.25,0.35);
+				Cookie ck1 = new Cookie("Chocolate Chip",6,3.99);
+				IceCream i = new IceCream("Pistachio",2,0.79);
+				Sundae s = new Sundae("Vanilla",3,0.69,"Hot Fudge",1.29);
+				Cookie ck2 = new Cookie("Oatmeal Raisin",2,3.45);
+				
+				//Adding Items to the order
+				orders.add(c1);
+				orders.add(c2);
+				orders.add(ck1);
+				orders.add(i);
+				orders.add(s);
+				orders.add(ck2);
 		
-		//Adding Items to the order
-		orders.add(c1);
-		orders.add(c2);
-		orders.add(ck1);
-		orders.add(i);
-		orders.add(s);
-		orders.add(ck2);
+		/******Begin Copy******/
+
+		Scanner sIn = new Scanner(System.in);    
+		String choice;
+		DessertItem orderItem;
+
+		boolean done = false;
+		while (!done) {
+		    System.out.println("\n1: Candy");
+		    System.out.println("2: Cookie");            
+		    System.out.println("3: Ice Cream");
+		    System.out.println("4: Sunday");
+
+		    System.out.print("\nWhat would you like to add to the order? (1-4, Enter for done): ");
+		    choice = sIn.nextLine();
+		    
+		    if (choice.equals("")) {
+		        done = true;
+		    } else {
+		        switch (choice) {
+		            case "1":            
+		                orderItem = userPromptCandy();
+		                orders.add(orderItem);
+		                System.out.printf("%n%s has been added to your order.%n",orderItem.getName());
+		                break;
+		            case "2":            
+		                orderItem = userPromptCookie();
+		                orders.add(orderItem);
+		                System.out.printf("%n%s has been added to your order.%n",orderItem.getName());
+		                break;
+		            case "3":            
+		                /*orderItem = userPromptIceCream();
+		                orders.add(orderItem);
+		                System.out.printf("%n%s has been added to your order.%n",orderItem.getName());*/
+		                break;
+		            case "4":            
+		                /*orderItem = userPromptSundae();
+		                orders.add(orderItem);
+		                System.out.printf("%n%s has been added to your order.%n",orderItem.getName());*/
+		                break;
+		            default:            
+		                System.out.println("Invalid response:  Please enter a choice from the menu (1-4)");
+		                break;
+		        }//end of switch (choice)
+		    }//end of if (choice.equals(""))
+		}//end of while (!done)
+		System.out.println("\n");
+
+		/******End Copy*****/
+		
+		
+		
 		
 		ArrayList<DessertItem>  orderItems = orders.getOrderList();
 		
@@ -34,4 +89,103 @@ public class DessertShop {
 		System.out.printf("%-25s$%-8.2f\n","Order Total",orders.orderCost() + orders.orderTax());
 		System.out.println("Total items in the order: "+ orders.itemCount());
 	}
+	private static DessertItem userPromptCandy() {
+		Scanner in = new Scanner(System.in);
+		boolean status = false;
+		String name="";
+		double candyWeigth=0.0;
+		double pricePerPound=0.0;
+		
+		do {
+			try {
+				System.out.println("Enter the type of candie: ");
+				name = in.nextLine();
+				status = true;
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Invalid response: Numbers are not allowed");
+			}	
+		}while(!status);
+		status = false;
+		do {
+			try {
+				System.out.println("Enter the weigth of candie: ");
+				candyWeigth = Double.parseDouble(in.nextLine());
+				status = true;
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Invalid response: Strings are not allowed");
+			}	
+		}while(!status);
+		status = false;
+		do {
+			try {
+				System.out.println("Enter the price per pound: ");
+				pricePerPound = Double.parseDouble(in.nextLine());
+				status = true;
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Invalid response: Strings are not allowed");
+			}	
+		}while(!status);
+		
+		/*in.close();*/
+
+		//Candy c = new Candy(name,candyWeigth,pricePerPound);
+		return new Candy(name,candyWeigth,pricePerPound);
+	}
+	private static DessertItem userPromptCookie() {
+		Scanner in = new Scanner(System.in);
+		boolean status = false;
+		String name="";
+		int cookieQty=0;
+		double pricePerDozen=0.0;
+		
+		do {
+			try {
+				System.out.println("Enter the type of cookie: ");
+				name = in.nextLine();
+				status = true;
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Invalid response: Numbers are not allowed");
+			}	
+		}while(!status);
+		status = false;
+		do {
+			try {
+				System.out.println("Enter the quantity purchased: ");
+				cookieQty = Integer.parseInt(in.nextLine());
+				status = true;
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Invalid response: Strings are not allowed");
+			}	
+		}while(!status);
+		status = false;
+		do {
+			try {
+				System.out.println("Enter the price per dozen: ");
+				pricePerDozen = Double.parseDouble(in.nextLine());
+				status = true;
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Invalid response: Strings are not allowed");
+			}	
+		}while(!status);
+		
+		/*in.close();*/
+
+		//Candy c = new Candy(name,candyWeigth,pricePerPound);
+		return new Cookie(name,cookieQty,pricePerDozen);
+	}
+	/*private static DessertItem userPromptCookie() {
+		
+	}
+	private static DessertItem userPromptIceCream() {
+		
+	}
+	private static DessertItem userPromptSundae() {
+		
+	}*/
 }
