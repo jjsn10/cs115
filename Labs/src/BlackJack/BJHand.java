b3602a3a104820aefcfcb912c6cd238988111be6
jjns10 <san14038@byui.edu>
2023-07-17 13:38:53 -0600

    BlackJack Final Version

--- Labs/src/BlackJack/BJHand.java
+++ Labs/src/BlackJack/BJHand.java
@@ -1,6 +1,7 @@
 package BlackJack;
 
 import BlackJackBase.PCard;
+import java.util.Scanner;
 import BlackJackBase.PHand;
 import java.util.ArrayList;
 
@@ -48,26 +49,77 @@ public class BJHand implements PHand{
 	public int getValue() {
 		// TODO Auto-generated method s
 		int sumNum =0;
+		Scanner iSN = new Scanner(System.in);
+			for(int i=0; i<getSize(); i++) {
+				System.out.println("Line 54: "+sumNum);
+				if(hand.get(i).getRank() > BJCard.TEN && hand.get(i).getRank() <= BJCard.KING) {
+					sumNum += 10;
+					/*System.out.println("Line 57:" +hand.get(i).getRank());
+					System.out.println("Line 58:" +sumNum);
+					iSN.nextLine();
+					System.out.println();*/
+				}
+				else if(hand.get(i).getRank() == BJCard.ACE) {
+					if(sumNum<11) {
+						sumNum += 11;
+						/*System.out.println("Line 65: "+hand.get(i).getRank());
+						System.out.println("Line 66: "+sumNum);
+						iSN.nextLine();
+						System.out.println();*/
+						
+					}else {
+						sumNum += 1;
+						/*System.out.println("Line 72: "+hand.get(i).getRank());
+						System.out.println("Line 73: " +sumNum);
+						iSN.nextLine();
+						System.out.println();*/
+						
+					}
+				}else {
+						sumNum += hand.get(i).getRank();
+						/*System.out.println("Line 79: "+hand.get(i).getRank());
+						System.out.println("Line 80: "+sumNum);
+						iSN.nextLine();
+						System.out.println();*/
+				}
+			}
 			
-			for(int i=0; i<getSize();i++) {
+			/*for(int i=0; i<getSize();i++) {
 				if(hand.get(i).getRank()>BJCard.TEN && hand.get(i).getRank()<=BJCard.KING) {
 					if(sumNum+10<=21) {
-						sumNum +=10;
+						sumNum = sumNum + 10;
+						System.out.println("Line 58: "+hand.get(i).getRank());
+						System.out.println("Line 59: "+sumNum);
+						iSN.nextLine();
+						System.out.println();
 					}
 				}else if(hand.get(i).getRank()==BJCard.ACE) {
 					if(sumNum<11) {
-						sumNum +=11;
+						sumNum = sumNum + 11;
+						System.out.println("Line 66: "+hand.get(i).getRank());
+						System.out.println("Line 67: "+sumNum);
+						iSN.nextLine();
+						System.out.println();	
+						
 					}else {
-						sumNum++;
+						sumNum = sumNum + 1;
+						System.out.println("Line 73: "+hand.get(i).getRank());
+						System.out.println("Line 74: " +sumNum);
+						iSN.nextLine();
+						System.out.println();
 						
 					}
 				}else {
 					if(sumNum+hand.get(i).getRank()<=21) {
-						sumNum +=hand.get(i).getRank();
+						sumNum = sumNum + hand.get(i).getRank();
+						System.out.println("Line 82: "+hand.get(i).getRank());
+						System.out.println("Line 83: "+sumNum);
+						iSN.nextLine();
+						System.out.println();
 					}
 				}
 				
-			}	
+			}	*/
 		return sumNum;
 	}
 
